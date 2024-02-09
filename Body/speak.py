ae6f6ff75e63730815c22853dc69fc49561cb6f5
Siddharth2605 <siddharthsurana2605@gmail.com>
2024-02-09 08:24:47 +0530

    initial commit

---
+++ Body/speak.py
@@ -0,0 +1,14 @@
+import pyttsx3
+
+def speak(text):
+    engine = pyttsx3.init('sapi5')
+    voices=engine.getProperty('voices')
+    engine.setProperty('voices',voices[1].id)
+    engine.setProperty('rate',170)
+    print("")
+    print(f"jarvis:{text}")
+    engine.say(text)
+    engine.runAndWait()
+
+
+speak("Hello, I am your Python speaking assistant created by siddharth")
