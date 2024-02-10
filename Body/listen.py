c1feddb55bd1b2c32c721dde462aceb7498568b4
Siddharth2605 <siddharthsurana2605@gmail.com>
2024-02-10 18:39:12 +0530

    Updated Listen.py and added voice.html

--- Body/listen.py
+++ Body/listen.py
@@ -1,34 +1,38 @@
-#pip install selenium
 from selenium import webdriver
-from selenium.webdriver.common. by import By
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from as import getcwd
+from os import getcwd
 import colorama
 from colorama import Fore, Back, Style
-colorama.init(autoreset.true)
+
+colorama.init(autoreset=True)
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--use-fake-u1-for-media-stream")
-chrome_options.add_argument("--headless=new")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
-website = f"C:\\Users\\factw\\Desktop\\JARVIS\Body\\voice.html"
+chrome_options.add_argument("--use-fake-ui-for-media-stream")
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+website = f"C:\\Users\\ADMIN\\Desktop\\FRIDAY\Body\\voice.html"
 
 driver.get(website)
 
-def listen():
-	print()
-	print(Fore.GREEN + "LISTENING . . .")
-	print()
-	driver.get(website)
-	driver.find_element(by-By.ID, value='start').click()
-	while 1
-		text=driver.find_element(by-By.ID, value='output').text()
-		if text != ''
-			print(Fore.BLUE+"YOU SAID : " + text)
-			print()
-			driver.find_element(by-By.ID, value 'end').click()
-			return text
-if _name=="main_"
-	while 1
-		listen()
+
+def Listen():
+    print()
+    print(Fore.MAGENTA + "LISTENING ... ")
+    print()
+    driver.get(website)
+    driver.find_element(By.ID, 'start').click()
+    while True:  # Changed from while 1 to while True
+        text = driver.find_element(By.ID, 'output').text
+        if text != "":
+            print(Fore.YELLOW + "YOU SAID : " + text)
+            print()
+            driver.find_element(By.ID, 'end').click()
+            return text
+
+
+if __name__ == "__main__":  # Corrected the if __name__ line
+    while True:  # Changed from while 1 to while True
+        Listen()
